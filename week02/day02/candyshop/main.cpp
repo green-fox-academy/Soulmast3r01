873fcbdabcfd0cca9c67b853badf3df19015e476
SoulMast3r01 <akoswb@gmail.com>
2018-11-23 13:18:27 +0100

    previous exercises practise

---
+++ week02/day02/candyshop/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+int main(int argc, char* args[])
+{
+    const std::vector<std::string> sweets = {"Cupcake", "Brownie", "Cake"};
+    std::vector<std::string> list = {"Cupcake", "Carrot", "Bread", "Brownie", "Lemon"};
+
+    // Accidentally we added "Carrot", "Lemon" and "Bread" to the list. 
+    // Your task is to remove them from the list. "sweets" vector can help you with this
+    // You should erase every element from "list" vector that is not in "sweets" vector.
+    // No, don't just remove the lines
+    // Create a method called filter() which takes the list as a parameter. 
+
+    // Expected output: Cupcake Brownie
+    for(const auto& sweet : filter(list, sweets))
+    {
+        std::cout << sweet << " ";
+    }
+
+    return 0;
+}
