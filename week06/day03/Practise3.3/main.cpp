97c33fdd122577abbd272816879879a75ea81617
SoulMast3r01 <akoswb@gmail.com>
2018-11-28 12:40:55 +0100

    left 3.3 exercise unready

---
+++ week06/day03/Practise3.3/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int unique(int numbers[], int size );
+int main() {
+    int numbers[8] = {1, 11, 34, 11, 52, 61, 1, 34};
+    int size = 8;
+    std::cout << unique(numbers,size) << std::endl;
+    return 0;
+}
+int unique(int numbers[], int size ){
+    for (int i = 0; i < 8; ++i) {
+        if(numbers[0] == numbers[i + 1] )
+            for (int j = 0; j <size -1; ++j) {
+
+            }
+    }
+
+}
