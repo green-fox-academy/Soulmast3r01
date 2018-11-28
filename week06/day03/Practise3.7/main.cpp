59324e308382d9f2ae00c8ec3c09ea776cb6f033
SoulMast3r01 <akoswb@gmail.com>
2018-11-28 17:27:52 +0100

    finished 3.7 exercise

---
+++ week06/day03/Practise3.7/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+std::string reverse(const std::string& text) {
+    std::string new1 = "";
+    int n = text.length();
+    for (int i = n; i > 0; i--)
+        new1
+}
+int main() {
+    std::string reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+    std::cout << reverse(reversed) << std::endl;
+    return 0;
+}
+
