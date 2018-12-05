724caafa8af81611c262c2ff7fd94ee91891d3d8
SoulMast3r01 <akoswb@gmail.com>
2018-12-05 15:31:02 +0100

    finished guess-the-number exercise

--- week07/day01/guess-the-number/main.c
+++ week07/day01/guess-the-number/main.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main() {
-    int number = rand() % 11;
-    int guess1;
-    scanf("%d", &guess1);
-    while()
+#include <time.h>
+
+int main()
+{
+    int random_num = 0;
+    int guessed_num = 0;
+
+
+    srand(time(NULL));
+    random_num = rand() % 10 + 1;
+    printf("Guess my number!\n ");
+
+    while(1)
+    {
+
+        scanf("%d", &guessed_num);
+        if (guessed_num == random_num)
+        {
+            printf("You guessed correctly!");
+            break;
+        }
+        if (guessed_num < random_num)
+            printf("The stored number is higher Guess again.\n ");
+
+        if (guessed_num > random_num)
+            printf("The stored number is lower Guess again. \n");
+
+    }
 
     return 0;
 }
