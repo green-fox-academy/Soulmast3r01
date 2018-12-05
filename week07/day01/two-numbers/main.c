653fbdb4ae8f5f930edd2ab84305840c2d1fd397
SoulMast3r01 <akoswb@gmail.com>
2018-12-05 13:44:07 +0100

    finished two-numbers exercise

---
+++ week07/day01/two-numbers/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 13;
+    int b = 22;
+    int sum = a + b;
+    int subdfhvbdhbvd = b - a;
+    float divided = (float)b / a;
+    int dividedresult = b % a;
+    int multiply = b * a;
+    printf("%d\n %d\n %.2f\n %d\n %d\n", sum, subdfhvbdhbvd, divided, dividedresult, multiply);
+    return 0;
+}
